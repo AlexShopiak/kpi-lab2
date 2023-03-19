fa32f61b4a07057a8cbb4fdc0ff4c8d474916eea
Alex Shopiak <alshop2004@gmail.com>
2023-03-19 17:05:39 +0100

    Implement stack

--- implementation.go
+++ implementation.go
@@ -2,8 +2,20 @@ package lab2
 
 import "fmt"
 
-// TODO: document this function.
-// PrefixToPostfix converts
+
+type Stack []string
+
+func (s *Stack) Pop() (string, bool) { //element, err
+	if len(*s) == 0 {
+		return "", true
+	} else {
+		index := len(*s) - 1
+		element := (*s)[index]
+		*s = (*s)[:index]
+		return element, false
+	}
+}
+
 func PrefixToPostfix(input string) (string, error) {
 	return "TODO", fmt.Errorf("TODO")
 }
