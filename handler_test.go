6c45109f1a2482d870f48d9271d258bda50d51f6
Alex Shopiak <alshop2004@gmail.com>
2023-03-20 19:24:57 +0100

    Format code

--- handler_test.go
+++ handler_test.go
@@ -9,6 +9,7 @@ import (
 
 func (s *MySuite) TestComputeHandler(c *C) {
 	buffer := bytes.NewBuffer(make([]byte, 0))
+	
 	handler := ComputeHandler{
 		Input:  strings.NewReader("* 5 2"),
 		Output: buffer,
@@ -18,8 +19,10 @@ func (s *MySuite) TestComputeHandler(c *C) {
 	c.Assert(err, Equals, nil)
 	c.Assert(buffer.String(), Equals, "10")
 }
- func (s *MySuite) TestComputeHandlerError(c *C) {
+
+func (s *MySuite) TestComputeHandlerError(c *C) {
 	buffer := bytes.NewBuffer(make([]byte, 0))
+	
 	handler := ComputeHandler{
 		Input:  strings.NewReader("- 5 5 6"),
 		Output: buffer,
